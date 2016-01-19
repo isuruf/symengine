e6f2547ecd63da6282236f8b4913976255ef0e4c
Isuru Fernando <isuruf@gmail.com>
2016-01-19 11:24:12 +0530

    Remove re-inialzation of mpfr_t

--- symengine/tests/eval/test_eval_mpfr.cpp
+++ symengine/tests/eval/test_eval_mpfr.cpp
@@ -42,7 +42,7 @@ TEST_CASE("precision: eval_mpfr", "[eval_mpfr]")
     REQUIRE(mpfr_cmp_d(a, 0.000000000149734291) == 1);
     REQUIRE(mpfr_cmp_d(a, 0.000000000149734292) == -1);
 
-    mpfr_init2(a, 100);
+    mpfr_set_prec(a, 100);
     s = mul(EulerGamma, integer(100000000));
     t = integer(57721566);
     r = div(sub(s, t), integer(100000000));
