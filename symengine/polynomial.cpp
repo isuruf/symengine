d49e9457e190880b3b847a7fd50a96a5fcd276a7
Matthew Luszczak <myluszczak@ucdavis.edu>
2016-03-05 17:47:55 -0800

    second hypothesis about segfaults on clang

--- symengine/polynomial.cpp
+++ symengine/polynomial.cpp
@@ -68,16 +68,16 @@ int UnivariateIntPolynomial::compare(const Basic &o) const
     return map_uint_mpz_compare(dict_, s.dict_);
 }
 
-RCP<const UnivariatePolynomial> UnivariatePolynomial::from_dict(const RCP<const Symbol> &var, map_uint_mpz &&d)
+RCP<const UnivariateIntPolynomial> UnivariateIntPolynomial::from_dict(const RCP<const Symbol> &var, map_uint_mpz &&d)
 {
     for(auto itter = d.begin(); itter != d.end(); itter++){
-        if(0_z == itter->second)
+        if(integer_class(0) == itter->second)
   	    d.erase(itter);
     }
     unsigned int degree = 0;
     if(!d.empty())
-        degree =  (--(d.end()))->first;
-    return make_rcp<const UnivariatePolynomial>(var,degree, std::move(d));
+        degree = (--(d.end()))->first;
+    return make_rcp<const UnivariateIntPolynomial>(var, degree, std::move(d));
 }
 
 RCP<const UnivariateIntPolynomial> from_vec(const RCP<const Symbol> &var, const std::vector<integer_class> &v){
@@ -159,15 +159,13 @@ bool UnivariatePolynomial::is_zero() const {
 }
 
 bool UnivariateIntPolynomial::is_one() const {
-    if (dict_.size() == 1)
-        if(dict_.begin()->second == 1 and dict_.begin()->first == 0)
+    if (dict_.size() == 1 and dict_.begin()->second == 1 and dict_.begin()->first == 0)
             return true;
     return false;
 }
 
 bool UnivariateIntPolynomial::is_minus_one() const {
-    if (dict_.size() == 1)
-        if(dict_.begin()->second == -1 and dict_.begin()->first == 0)
+    if (dict_.size() == 1 and dict_.begin()->second == -1 and dict_.begin()->first == 0)
             return true;
     return false;
 }
@@ -181,22 +179,19 @@ bool UnivariatePolynomial::is_integer() const {
 }
 
 bool UnivariateIntPolynomial::is_symbol() const {
-    if (dict_.size() == 1)
-        if(dict_.begin()->first == 1 and dict_.begin()->second == 1)
+    if (dict_.size() == 1 and dict_.begin()->first == 1 and dict_.begin()->second == 1)
             return true;
     return false;
 }
 
 bool UnivariateIntPolynomial::is_mul() const {
-    if (dict_.size() == 1)
-        if(dict_.begin()->first != 0 and dict_.begin()->second != 1 and dict_.begin()->second != 0)
+    if (dict_.size() == 1 and dict_.begin()->first != 0 and dict_.begin()->second != 1 and dict_.begin()->second != 0)
             return true;
     return false;
 }
 
 bool UnivariateIntPolynomial::is_pow() const {
-    if (dict_.size() == 1)
-        if(dict_.begin()->second == 1 and dict_.begin()->first != 1 and dict_.begin()->first != 0)
+    if (dict_.size() == 1 and dict_.begin()->second == 1 and dict_.begin()->first != 1 and dict_.begin()->first != 0)
             return true;
     return false;
 }
