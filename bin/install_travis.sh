0c163ca53d9f93b69bcc396dd021eaf6d79ec184
Isuru Fernando <isuruf@gmail.com>
2016-06-09 15:09:52 +0530

    Test

--- bin/install_travis.sh
+++ bin/install_travis.sh
@@ -98,7 +98,7 @@ if [[ "${WITH_MPC}" == "yes" ]]; then
     fi
 fi
 if [[ "${WITH_PIRANHA}" == "yes" ]]; then
-    git clone https://github.com/bluescarni/piranha;
+    git clone https://github.com/isuruf/piranha;
     cd piranha && mkdir build && cd build;
     cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$our_install_dir -DBUILD_TESTS=no ../ && make -j8 install && cd ../..;
 fi
