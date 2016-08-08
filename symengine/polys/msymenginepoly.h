7c9a6e08b45c5aa363b1bd1739a6d5cce702b62a
Isuru Fernando <isuruf@gmail.com>
2016-08-08 07:07:39 +0530

    MPoly and GaloisField

--- symengine/polys/msymenginepoly.h
+++ symengine/polys/msymenginepoly.h
@@ -9,6 +9,10 @@
 namespace SymEngine
 {
 
+class MPoly : public Basic
+{
+};
+
 template <typename Vec, typename Value, typename Wrapper>
 class UDictWrapper
 {
@@ -332,7 +336,7 @@ public:
 };
 
 template <typename Container, typename Poly>
-class MSymEnginePoly : public Basic
+class MSymEnginePoly : public MPoly
 {
 public:
     Container poly_;
